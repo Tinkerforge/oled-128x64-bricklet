d4e86c58fcdd98949aef304e9c9ffb53de1305e9
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2015-08-03 16:51:58 +0200

    Adding Perl examples

---
+++ software/examples/perl/example_hello_world.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+
+use Tinkerforge::IPConnection;
+use Tinkerforge::BrickletOLED128x64;
+
+use constant HOST => 'localhost';
+use constant PORT => 4223;
+use constant UID => 'XYZ'; # Change to your UID
+
+my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
+my $oled = Tinkerforge::BrickletOLED128x64->new(&UID, $ipcon); # Create device object
+
+$ipcon->connect(&HOST, &PORT); # Connect to brickd
+# Don't use device before $ipcon is connected
+
+#Clear display
+$oled->clear_display();
+
+# Write "Hello World" starting from upper left corner of the screen
+$oled->write_line(0, 0, 'Hello World');
+
+print "Press any key to exit...\n";
+<STDIN>;
+$ipcon->disconnect();
