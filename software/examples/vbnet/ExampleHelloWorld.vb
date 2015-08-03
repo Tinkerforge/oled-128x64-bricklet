b0569e60b450a64b198154d7b81caf114f8e7f9b
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2015-08-03 18:03:54 +0200

    Adding VB.NET examples

---
+++ software/examples/vbnet/ExampleHelloWorld.vb
@@ -0,0 +1,25 @@
+Imports Tinkerforge
+
+Module ExampleHelloWorld
+    Const HOST As String = "localhost"
+    Const PORT As Integer = 4223
+    Const UID As String = "XYZ" ' Change to your UID
+
+    Sub Main()
+        Dim ipcon As New IPConnection() ' Create IP connection
+        Dim oled As New BrickletOLED64x48(UID, ipcon) ' Create device object
+
+        ipcon.Connect(HOST, PORT) ' Connect to brickd
+        ' Don't use device before ipcon is connected
+
+        ' Clear display
+        oled.ClearDisplay()
+
+        ' Write "Hello World" starting from upper left corner of the screen
+        oled.WriteLine(0, 0, "Hello World")
+
+        System.Console.WriteLine("Press key to exit")
+        System.Console.ReadLine()
+        ipcon.Disconnect()
+    End Sub
+End Module
