7187cf41a7d5094a823f218f2067ba27ac55f9ae
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2015-08-03 15:38:47 +0200

    Adding JavaScript examples

---
+++ software/examples/javascript/ExampleHelloWorld.js
@@ -0,0 +1,32 @@
+var Tinkerforge = require('tinkerforge.js');
+
+var HOST = 'localhost';
+var PORT = 4223;
+var UID = 'XYZ'; // Change to your UID
+
+var ipcon = new Tinkerforge.IPConnection(); // Create IP connection
+var oled = new Tinkerforge.BrickletOLED128x64(UID, ipcon); // Create device object
+
+ipcon.connect(HOST, PORT,
+    function(error) {
+        console.log('Error: '+error);
+    }
+); // Connect to brickd
+// Don't use device before ipcon is connected
+
+ipcon.on(Tinkerforge.IPConnection.CALLBACK_CONNECTED,
+    function(connectReason) {
+        // Clear display
+        oled.clearDisplay();
+        // Write "Hello World" starting from upper left corner of the screen
+        oled.writeLine(0, 0, 'Hello World');
+    }
+);
+
+console.log("Press any key to exit ...");
+process.stdin.on('data',
+    function(data) {
+        ipcon.disconnect();
+        process.exit(0);
+    }
+);
