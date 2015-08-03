140274e78885583634bd570b17065698107bc7ee
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2015-08-03 17:21:09 +0200

    Adding Ruby examples

---
+++ software/examples/ruby/example_hello_world.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+# -*- ruby encoding: utf-8 -*-
+
+require 'tinkerforge/ip_connection'
+require 'tinkerforge/bricklet_oled_128x64'
+
+include Tinkerforge
+
+HOST = 'localhost'
+PORT = 4223
+UID = 'XYZ' # Change to your UID
+
+ipcon = IPConnection.new # Create IP connection
+oled = BrickletOLED128x64.new UID, ipcon # Create device object
+
+ipcon.connect HOST, PORT # Connect to brickd
+# Don't use device before ipcon is connected
+
+# Clear display
+oled.clear_display
+
+# Write "Hello World" starting from upper left corner of the screen
+oled.write_line 0, 0, 'Hello World'
+
+puts 'Press key to exit'
+$stdin.gets
+ipcon.disconnect
---
+++ software/examples/ruby/example_pixel_matrix.rb
@@ -0,0 +1,89 @@
+#!/usr/bin/env ruby
+# -*- ruby encoding: utf-8 -*-
+
+require 'tinkerforge/ip_connection'
+require 'tinkerforge/bricklet_oled_128x64'
+
+include Tinkerforge
+
+HOST = 'localhost'
+PORT = 4223
+UID = 'XYZ' # Change to your UID
+SCREEN_WIDTH = 128
+SCREEN_HEIGHT = 64
+
+def draw_matrix(oled, pixels)
+	column = []
+	column_write = []
+
+	for i in 0..SCREEN_HEIGHT/8 - 1
+		column[i] = []
+
+		for j in 0..SCREEN_WIDTH-1
+			page = 0
+
+			for k in 0..7
+				if pixels[i*8 + k][j] == true
+					page |= 1 << k
+				end
+			end
+
+			column[i][j] = page
+		end
+	end
+
+	oled.new_window(0, SCREEN_WIDTH-1, 0, 7)
+
+	for i in 0..SCREEN_HEIGHT/8 - 1
+		l = 0
+		for j in 0..(SCREEN_WIDTH/2) - 1
+			column_write[l] = column[i][j]
+			l = l + 1
+		end
+		oled.write(column_write)
+		l = 0
+		for k in SCREEN_WIDTH/2..(SCREEN_WIDTH/2) - 1
+			column_write[l] = column[i][k]
+			l = l + 1
+		end
+		oled.write(column_write)
+	end
+end
+
+ipcon = IPConnection.new # Create IP connection
+oled = BrickletOLED128x64.new UID, ipcon # Create device object
+
+ipcon.connect HOST, PORT # Connect to brickd
+# Don't use device before ipcon is connected
+
+# Clear display
+oled.clear_display
+
+# Pixel matrix with all pixels turned off
+pixel_matrix = []
+
+for i in 0..SCREEN_HEIGHT-1
+	pixel_matrix[i] = []
+
+	for j in 0..SCREEN_WIDTH-1
+		pixel_matrix[i][j] = false
+	end
+end
+
+# Draw check pattern
+for w in 0..SCREEN_WIDTH-1
+	for h in 0..SCREEN_HEIGHT-1
+		if w/5 % 2 == 0
+			pixel_matrix[h][w] = true
+		end
+		if h/5 % 2 == 0
+			pixel_matrix[h][w] = true
+		end
+	end
+end
+
+draw_matrix(oled, pixel_matrix)
+
+puts 'Press key to exit'
+$stdin.gets
+ipcon.disconnect
