e492e3dfca6672990290087fa0650f7c4466deab
Matthias Bolte <matthias@tinkerforge.com>
2015-08-12 11:30:18 +0200

    Fix minor warnings in C/C++ examples

--- software/examples/c/example_pixel_matrix.c
+++ software/examples/c/example_pixel_matrix.c
@@ -49,7 +49,7 @@ void draw_matrix(OLED128x64 *oled, bool (*pixels)[SCREEN_WIDTH]) {
 	}
 }
 
-int main() {
+int main(void) {
 	// Create IP connection
 	IPConnection ipcon;
 	ipcon_create(&ipcon);
@@ -67,7 +67,7 @@ int main() {
 	// Connect to brickd
 	if(ipcon_connect(&ipcon, HOST, PORT) < 0) {
 		fprintf(stderr, "Could not connect\n");
-		exit(1);
+		return 1;
 	}
 	// Don't use device before ipcon is connected
 
@@ -99,4 +99,5 @@ int main() {
 	printf("Press key to exit\n");
 	getchar();
 	ipcon_destroy(&ipcon); // Calls ipcon_disconnect internally
+	return 0;
 }
