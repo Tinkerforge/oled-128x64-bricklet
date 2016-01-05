89177018b6c78458ce55e46ff827a204bca023e2
Matthias Bolte <matthias@tinkerforge.com>
2016-01-05 18:15:15 +0100

    python: Get image from command line

--- software/examples/python/example_load_image.py
+++ software/examples/python/example_load_image.py
@@ -1,40 +1,39 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-  
+# -*- coding: utf-8 -*-
 
 HOST = "localhost"
 PORT = 4223
 UID = "XYZ" # Change to your UID
-SCREEN_WIDTH = 128
-SCREEN_HEIGHT = 64
+WIDTH = 128
+HEIGHT = 64
 
+import sys
 from PIL import Image
 from tinkerforge.ip_connection import IPConnection
 from tinkerforge.bricklet_oled_128x64 import BrickletOLED128x64, OLED128x64
 
-def draw_matrix(pixels):
-    column_index = 0
-    column = []
+def draw_matrix(oled, pixels):
+    pages = []
 
-    for i in range(SCREEN_HEIGHT//8):
-        for j in range(SCREEN_WIDTH):
+    # Convert pixels into pages
+    for row in range(HEIGHT // 8):
+        pages.append([])
+
+        for column in range(WIDTH):
             page = 0
 
-            for k in range(8):
-                if pixels[i*8 + k][j] == True:
-                    page |= 1 << k
+            for bit in range(8):
+                if pixels[(row * 8) + bit][column]:
+                    page |= 1 << bit
 
-            if len(column) <= column_index:
-                column.append([]) 
+            pages[row].append(page)
 
-            column[column_index].append(page)
+    # Write all pages
+    oled.new_window(0, WIDTH - 1, 0, HEIGHT // 8 - 1)
 
-            if len(column[column_index]) == SCREEN_HEIGHT:
-                column_index += 1
-
-        oled.new_window(0, SCREEN_WIDTH-1, 0, 7)
-
-        for i in range(len(column)):
-            oled.write(column[i])
+    for row in range(HEIGHT // 8):
+        for column in range(0, WIDTH, 64):
+            oled.write(pages[row][column:column + 64])
 
 if __name__ == "__main__":
     ipcon = IPConnection() # Create IP connection
@@ -43,23 +42,25 @@ if __name__ == "__main__":
     ipcon.connect(HOST, PORT) # Connect to brickd
     # Don't use device before ipcon is connected
 
-    image = Image.open("./tf_logo_128x64.png")
-    width, height = image.size
-    #data = [image.getpixel((x, y)) for x in range(width) for y in range(height)]
-
-    # Clear display and setup window to draw from
+    # Clear display
     oled.clear_display()
-    oled.new_window(0, 127, 0, 7)
-    
-    # Boolean matrix with all pixels turned off
-    pixel_matrix = [[False]*SCREEN_WIDTH for i in range(SCREEN_HEIGHT)]
-    
-    for w in range(width):
-        for h in range(height):
-            if image.getpixel((w, h)) == 255:
-                pixel_matrix[h][w] = True
 
-    draw_matrix(pixel_matrix)
+    # Convert image to black/white pixels
+    image = Image.open(sys.argv[1])
+    pixels = []
+
+    for row in range(HEIGHT):
+        pixels.append([])
+
+        for column in range(WIDTH):
+            if column < image.size[0] and row < image.size[1]:
+                pixel = image.getpixel((column, row)) > 0
+            else:
+                pixel = False
+
+            pixels[row].append(pixel)
+
+    draw_matrix(oled, pixels)
 
     raw_input('Press key to exit\n') # Use input() in Python 3
     ipcon.disconnect()
