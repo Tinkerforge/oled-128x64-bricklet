0b541c516cf90105a5422d5377f4f2bb652aaeab
Erik Fleckstein <erik@tinkerforge.com>
2018-12-18 14:58:26 +0100

    go: Change import path.

--- software/examples/go/example_pixel_matrix.go
+++ software/examples/go/example_pixel_matrix.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"tinkerforge/ipconnection"
-    "tinkerforge/oled_128x64_bricklet"
+	"github.com/tinkerforge/go-api-bindings/ipconnection"
+    "github.com/tinkerforge/go-api-bindings/oled_128x64_bricklet"
 )
 
 const ADDR string = "localhost:4223"
