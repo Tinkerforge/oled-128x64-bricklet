433916b0e98b813386fa8641b48167b402f94f34
Erik Fleckstein <erik@tinkerforge.com>
2020-01-14 13:31:45 +0100

    examples: Use python3 input by default.

--- software/examples/python/example_load_image.py
+++ software/examples/python/example_load_image.py
@@ -72,5 +72,5 @@ if __name__ == "__main__":
 
     draw_matrix(oled, 0, 0, WIDTH, HEIGHT, pixels)
 
-    raw_input('Press key to exit\n') # Use input() in Python 3
+    input("Press key to exit\n") # Use raw_input() in Python 2
     ipcon.disconnect()
